fb26f3a2bea952c7eecd9fa8ba9fe02c5aadfc41
Astorian <royalgamingarmy@gmail.com>
2013-03-28 13:45:30 +0200

    Create ItemIDs.java

---
+++ src/minecraft/moony/compactcrafting/lib/ItemIDs.java
@@ -0,0 +1,25 @@
+package moony.compactcrafting;
+
+public class ItemIDs {
+
+    public static int C1IronPickaxeID;
+    public static int C1DiamondPickaxeID;
+    public static int C1GoldPickaxeID;
+    public static int C1StonePickaxeID;
+    public static int C1WoodenPickaxeID;
+    public static int C2IronPickaxeID;
+    public static int C2DiamondPickaxeID;
+    public static int C2GoldenPickaxeID;
+    public static int C2StonePickaxeID;
+    public static int C2WoodenPickaxeID;
+    public static int C1IronAxeID;
+    public static int C1DiamondAxeID;
+    public static int C1GoldAxeID;
+    public static int C1StoneAxeID;
+    public static int C1WoodenAxeID;
+    public static int C2IronAxeID;
+    public static int C2DiamondAxeID;
+    public static int C2GoldAxeID;
+    public static int C2StoneAxeID;
+    public static int C2WoodenAxeID;
+}
