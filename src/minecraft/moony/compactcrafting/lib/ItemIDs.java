3a5138d84d61cb5e6fd5a851157715eba6b48729
Astorian <royalgamingarmy@gmail.com>
2013-03-29 15:36:10 +0200

    Added default IDs

--- src/minecraft/moony/compactcrafting/lib/ItemIDs.java
+++ src/minecraft/moony/compactcrafting/lib/ItemIDs.java
@@ -22,4 +22,25 @@ public class ItemIDs {
     public static int C2GoldAxeID;
     public static int C2StoneAxeID;
     public static int C2WoodenAxeID;
+
+    public static int C1IronPickaxeID_Default = 4200;
+    public static int C1DiamondPickaxeID_Default = 4201;
+    public static int C1GoldPickaxeID_Default = 4202;
+    public static int C1StonePickaxeID_Default = 4203;
+    public static int C1WoodenPickaxeID_Default = 4204;
+    public static int C2IronPickaxeID_Default = 4205;
+    public static int C2DiamondPickaxeID_Default = 4206;
+    public static int C2GoldenPickaxeID_Default = 4207;
+    public static int C2StonePickaxeID_Default = 4208;
+    public static int C2WoodenPickaxeID_Default = 4209;
+    public static int C1IronAxeID_Default = 4210;
+    public static int C1DiamondAxeID_Default = 4211;
+    public static int C1GoldAxeID_Default = 4212;
+    public static int C1StoneAxeID_Default = 4214;
+    public static int C1WoodenAxeID_Default = 4215;
+    public static int C2IronAxeID_Default = 4216;
+    public static int C2DiamondAxeID_Default = 4217;
+    public static int C2GoldAxeID_Default = 4218;
+    public static int C2StoneAxeID_Default = 4219;
+    public static int C2WoodenAxeID_Default = 4220;
 }
