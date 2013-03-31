3bb7d2bfc3157118c271725824696b6d2772d438
Astorian <royalgamingarmy@gmail.com>
2013-03-31 14:21:58 +0300

    Create ConfigurationHandler.java

---
+++ src/minecraft/moony/compactcrafting/core/handler/ConfigurationHandler.java
@@ -0,0 +1,20 @@
+package moony.compactcrafting.core.handler;
+
+import net.minecraftforge.common.Configuration;
+
+import moony.compactcrafting.lib.ItemIDs;
+import moony.compactcrafting.lib.BlockIDs;
+
+public class ConfigurationHandler {
+
+    public static Configuration config;
+
+    public static void init(File configFile) {
+
+        config = new Configuration(configFile);
+
+        try
+
+    }
+
+}
