b2318e16785aca950e0e1dca6ccd38814c45e8a3
Astorian <royalgamingarmy@gmail.com>
2013-03-31 14:14:23 +0300

    Update BlockIDs.java

--- src/minecraft/moony/compactcrafting/lib/BlockIDs.java
+++ src/minecraft/moony/compactcrafting/lib/BlockIDs.java
@@ -3,5 +3,17 @@ package moony.compactcrafting.lib;
 public class BlockIDs {
 
     public static int CompactCobblestoneID;
+    public static int CompactCoalBlockID;
+	public static int CompactNetherrackID;
+	public static int CompactDirtID;
+    public static int CompactSandID;
+    public static int CompactGlassID;
+    
+    public static int CompactCobblestoneID_Default = 711;
+    public static int CompactCoalBlockID_Default = 712;
+    public static int CompactNetherrackID_Default = 713;
+    public static int CompactDirtID_Default = 714;
+    public static int CompactSandID_Default = 715;
+    public static int CompactGlassID_Default = 716;
 
 }
